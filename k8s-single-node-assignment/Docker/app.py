e47fbe0fb4754d65c4adf607cceab58716a83a45
saikiran.kotte <sai.kotte@csi.com>
2026-03-03 18:05:04 +0530

    faced crashloopBackOff

--- k8s-single-node-assignment/Docker/app.py
+++ k8s-single-node-assignment/Docker/app.py
@@ -1,1 +1,7 @@
-print("Hello World from Secure Container!")
+import time
+
+print("Hello World from a secure Python container!")
+
+# This keeps the container active so Kubernetes sees it as 'Ready'
+while True:
+    time.sleep(30)
